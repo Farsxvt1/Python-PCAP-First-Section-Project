c16bae05f14e48b512a00719ee03f784e9980345
Fares-Alsleem <alslymfars9@gmail.com>
2025-09-14 07:28:31 +0300

    Update main.py

--- First-project/restaurant/main.py
+++ First-project/restaurant/main.py
@@ -1,23 +1,11 @@
 menu = {}
 
-from restaurant.chef import add_dish, remove_dish, update_price, place_order, display_menu
-
-# Adding dishes to the menu
-add_dish(menu, "Pizza", 12.99)
-add_dish(menu, "Pasta", 9.99)
-add_dish(menu, "Salad", 6.99)
-
-# Displaying the menu
-display_menu(menu)
-
-# Placing an order for Pizza
-place_order(menu, "Pizza")
-
-# Displaying the menu after placing the order
-display_menu(menu)
-
-# Updating the price of Pizza
-update_price(menu, "Pizza", 13.99)
-
-# Displaying the menu after price update
-display_menu(menu)
+from restaurant.chef import add_dish as add, remove_dish as rem ,update_price as up, place_order as pla, display_menu as dis
+add(menu, "Pizza", 12.99)
+add(menu, "Pasta", 9.99)
+add(menu, "Salad", 6.99)
+dis(menu)
+pla(menu, "Pizza")
+dis(menu)
+up(menu, "Pizza", 13.99)
+dis(menu)
